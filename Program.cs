6f1b76d5ae1d05d9e3e1a4bf22252fe157a5b107
Andrea Restle-Lay <arrestle@ncsu.edu>
2021-01-17 13:39:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var pond = new HelloRequest { Name = "Baby Frog", Location = "Pollywog Bog" };
+            var dog = new HelloRequest { Name = "Logan the Magnificent", Location = "Dog Bed in the Corner" };
+
+            var reply = await client.SayHelloAsync(pond);
+            Console.WriteLine("Greeting 1: " + reply.Message);
+            reply = await client.SayHelloAsync(  dog);
+            Console.WriteLine("Greeting 2: " + reply.Message);
+
+            reply = await client.SayGoodbyeAsync( pond );
+            Console.WriteLine("Greeting 3: " + reply.Message);
+
+            reply = await client.SayGoodbyeAsync(dog);
+            Console.WriteLine("Greeting 4: " + reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
